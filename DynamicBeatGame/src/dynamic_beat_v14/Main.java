00c34db07a5d0b7b6e69527c353e8d9fc3857487
mykim2020 <mykim8710@gmail.com>
2020-05-01 17:47:02 +0900

    자바 리듬게임 구현 V14 - 곡별로 박자에 맞게끔 노트 찍기 구현

---
+++ DynamicBeatGame/src/dynamic_beat_v14/Main.java
@@ -0,0 +1,15 @@
+package dynamic_beat_v14;
+
+public class Main {
+	public static final int SCREEN_WIDTH = 1280;		// 스크린 : 게임창 너비
+	public static final int SCREEN_HEIGHT = 720;		// 스크린 : 게임창 높이
+	public static final int NOTE_SPEED = 3;				// 노트 떨어지는 스피드
+	public static final int SLEEP_TIME = 10;				// 노트가 떨어지는 주기
+	public static final int REACH_TIME = 2;				// 노트가 생성된 후 판정바에 도달할때까지의 시간
+	
+	public static void main(String[] args) {
+		
+		DynamicBeat db =  new DynamicBeat();
+
+	}
+}
